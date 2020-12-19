744edf40044f37b90ad9d44830e1eaf6fe0e523a
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-12-19 18:21:40 +0530

    removed parking lots and modified behaviour of parkinglot

--- src/parkingLot/ParkingLot.java
+++ src/parkingLot/ParkingLot.java
@@ -1,23 +1,23 @@
 package parkingLot;
 
+import java.util.ArrayList;
+
 public class ParkingLot {
+
+    private final ArrayList<Integer> slots;
     private final int capacity;
-    private int occupiedSpaces;
-    
+
     public ParkingLot(int capacity) {
+        this.slots = new ArrayList<>(capacity);
         this.capacity = capacity;
-        this.occupiedSpaces = 0;
     }
-    
-    public boolean park() {
-        if (this.isFull()) {
-            return false;
-        }
-        this.occupiedSpaces += 1;
-        return true;
+
+    public boolean park(int carId) {
+        if (this.isFull()) return false;
+        return this.slots.add(carId);
     }
-    
+
     public boolean isFull() {
-        return this.occupiedSpaces == this.capacity;
+        return this.capacity == this.slots.size();
     }
 }
--- test/parkingLot/ParkingLotTest.java
+++ test/parkingLot/ParkingLotTest.java
@@ -2,29 +2,30 @@ package parkingLot;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ParkingLotTest {
     @Test
     public void shouldParkACarWhenParkingLotHasEmptySpaces() {
         ParkingLot parkingLot = new ParkingLot(5);
-        assertTrue(parkingLot.park());
+        assertTrue(parkingLot.park(1));
     }
-    
+
     @Test
     public void shouldNotParkACarWhenParkingLotHasNoEmptySpaces() {
         ParkingLot parkingLot = new ParkingLot(1);
-        parkingLot.park();
-        assertFalse(parkingLot.park());
+        parkingLot.park(1);
+        assertFalse(parkingLot.park(2));
     }
-    
+
     @Test
     public void shouldTellIfTheParkingLotIsFull() {
         ParkingLot parkingLot = new ParkingLot(1);
-        parkingLot.park();
+        parkingLot.park(1);
         assertTrue(parkingLot.isFull());
     }
-    
+
     @Test
     public void shouldTellIfTheParkingLotIsNotFull() {
         ParkingLot parkingLot = new ParkingLot(1);
